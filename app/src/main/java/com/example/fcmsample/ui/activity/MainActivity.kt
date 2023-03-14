8be7bae2533437325d986840ffb150e2728346c9
JSangYeon <sju01181@naver.com>
2023-03-14 18:15:28 +0900

    FCM 토큰 수신 및 수신 시 Toast 추가

--- app/src/main/java/com/example/fcmsample/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/fcmsample/ui/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.fcmsample.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.fcmsample.R
 import com.example.fcmsample.databinding.ActivityMainBinding
 import com.example.fcmsample.ui.base.BaseActivity
 import com.example.fcmsample.ui.viewmodel.MainViewModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +22,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         Log.d(logTag, "fcmSample")
 
         binding.mainViewModel = _mainViewModel
+
+
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task -> // FCM 토큰 구하기
+            if (!task.isSuccessful) {
+                Log.w(logTag, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            Log.d(logTag, "token : $token")
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
 
 
     }
